d496c3b0215f97722175f558870b85657ed66494
believelody <believelody@gmail.com>
2021-02-10 14:55:18 +0100

    todoapp part2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # This a todo app tutorial series in reagent from AutoScreencast's youtube channel
 
 ### [Link to part1 here](https://www.youtube.com/watch?v=aGk-fRW4t-U)
+### [Link to part2 here](https://www.youtube.com/watch?v=_HNzDguFqnw)
--- src/main/todomvc/app/core.cljs
+++ src/main/todomvc/app/core.cljs
@@ -1,19 +1,20 @@
 (ns todomvc.app.core
   (:require [reagent.dom :as rdom]
             [reagent.core :as r]
-            [cljs.pprint :refer [pprint]]))
+            [cljs.pprint :refer [pprint]]
+            [clojure.string :as str]))
 
 ;; --- App State ---
 
-(def initial-todos {1 {:id 1, :title "Do laundry", :done false}
-                    3 {:id 3, :title "Buy groceries", :done false}
-                    2 {:id 2, :title "Wash dishes", :done true}})
+;; (def initial-todos {1 {:id 1, :title "Do laundry", :done false}
+;;                     3 {:id 3, :title "Buy groceries", :done false}
+;;                     2 {:id 2, :title "Wash dishes", :done true}})
 
-(def initial-todos-sorted (into (sorted-map) initial-todos))
+;; (def initial-todos-sorted (into (sorted-map) initial-todos))
 
-(defonce todos (r/atom initial-todos-sorted))
+(defonce todos (r/atom (sorted-map)))
 
-(defonce counter (r/atom (count initial-todos)))
+(defonce counter (r/atom 0))
 
 ;; --- Watch the State ---
 
@@ -29,14 +30,45 @@
         new-todo {:id id, :title text, :done false}]
     (swap! todos assoc id new-todo)))
 
+(defn save-todo [id title]
+  (swap! todos assoc-in [id :title] title))
+
+(defn delete-todo [id]
+  (swap! todos dissoc id))
+
+(defn toggle-done [id]
+  (swap! todos update-in [id :done] not))
+
+(defn mmap [m f g]
+  (->> m
+       (f g)
+       (into (empty m))))
+
+(defn complete-all-toggle [is-complete?]
+  (let [g #(assoc-in % [1 :done] is-complete?)]
+    (swap! todos mmap map g)))
+
+(defn clear-completed []
+  (let [g #(get-in % [1 :done])]
+    (swap! todos mmap remove g)))
+
+;; --- Init App with Sample Data ---
+
+(defonce init (do
+                (add-todo "Do laundry")
+                (add-todo "Wash dishes")
+                (add-todo "Buy groceries")))
+
 ;; --- Views ---
 
-(defn todo-input []
-  (let [input-text (r/atom "")
+(defn todo-input [{:keys [class placeholder title on-save on-stop]}]
+  (let [input-text (r/atom title)
         update-text #(reset! input-text %)
-        reset-text #(reset! input-text "")
-        save #(do
-                (add-todo @input-text)
+        reset-text #(do (reset! input-text "")
+                        (when on-stop (on-stop)))
+        save #(let [trimmed-text (-> @input-text str str/trim)]
+                (when-not (empty? trimmed-text)
+                  (on-save trimmed-text))
                 (reset-text))
         key-pressed #(case %
                        "Enter" (save)
@@ -44,44 +76,91 @@
                        "Escape" (reset-text)
                        nil)]
     (fn []
-      [:input {:class "new-todo"
-               :placeholder "Todo input"
+      [:input {:class class
+               :auto-focus true
+               :placeholder placeholder
                :type "text"
                :value @input-text
                :on-blur save
                :on-key-down #(key-pressed (.. % -key))
                :on-change #(update-text (.. % -target -value))}])))
 
-(defn todo-item [{:keys [title]}]
-  [:li
-   [:div.view
-    [:label title]]])
+(defn todo-item [_]
+  (let [editing (r/atom false)]
+    (fn [{:keys [id title done]}]
+      [:li {:class (str (when done "completed")
+                        (when @editing "editing"))}
+       [:div.view
+        [:input {:class "toggle"
+                 :type "checkbox"
+                 :checked done
+                 :on-change #(toggle-done id)}]
+        [:label {:on-double-click #(reset! editing true)} title]
+        [:button.destroy {:on-click #(delete-todo id)}]]
+       (when @editing
+         [todo-input {:class "edit"
+                      :title title
+                      :on-save (fn [text] (save-todo id text))
+                      :on-stop #(reset! editing false)}])])))
 
-(defn task-list []
-  (let [items (vals @todos)]
+(defn task-list [showing]
+  (let [items (vals @todos)
+        filter-fn (case @showing
+                    :done :done
+                    :active (complement :done)
+                    :all identity)
+        visible-items (filter filter-fn items)
+        all-complete? (every? :done items)]
     [:section.main
+     [:input  {:id "toggle-all"
+               :class "toggle-all"
+               :type "checkbox"
+               :checked all-complete?
+               :on-change #(complete-all-toggle (not all-complete?))}]
+     [:label {:for "toggle-all"} "Mark all as complete"]
      [:ul.todo-list
-      (for [todo items]
+      (for [todo visible-items]
         ^{:key (:id todo)}[todo-item todo])]]))
 
-(defn footer-controls []
-  [:footer.footer
-   [:div "Footer controls"]])
+(defn footer-controls [showing]
+  (let [items (vals @todos)
+        done-count (count (filter :done items))
+        active-count (- (count items) done-count)
+        props-for (fn [kw]
+                    {:class (when (= kw @showing) "selected")
+                     :on-click #(reset! showing kw)
+                     :href "#"})]
+    [:footer.footer
+     [:span.todo-count
+      [:strong active-count] " " (case active-count
+                               1 "item"
+                               "items") " left"]
+     [:ul.filters
+      [:li [:a (props-for :all) "All"]]
+      [:li [:a (props-for :active) "Active"]]
+      [:li [:a (props-for :done) "Completed"]]]
+     (when (pos? done-count)
+       [:button.clear-completed {:on-click clear-completed} "Clear completed"])]))
 
 (defn task-entry []
   [:header.header
    [:h1 "todos"
-    [todo-input]]])
+    [todo-input {:class "new-todo"
+                 :placeholder "What needs to be done ?"
+                 :on-save add-todo}]]])
 
 (defn todo-app []
-  [:div
-   [:section.todoapp
-    [task-entry]
-    [:div
-     [task-list]
-     [footer-controls]]]
-   [:footer.info
-    [:p "Footer info"]]])
+  (let [showing (r/atom :all)]
+    (fn []
+      [:div
+       [:section.todoapp
+        [task-entry]
+        (when (seq @todos)
+          [:div
+           [task-list showing]
+           [footer-controls showing]])]
+       [:footer.info
+        [:p "Double-click to edit a todo"]]])))
 
 ;; --- Render ---
 
